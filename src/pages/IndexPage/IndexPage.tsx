18aacf7726f94b27c6faefdc29e181d51aa60a18
Mohammed Alghazali <mohammedalghazali@alMacBook-Air-alkhas-bMohammed.local>
2025-07-04 12:11:14 +0300

    Add Carlos

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -32,7 +32,7 @@ export const IndexPage: FC = () => {
   return (
     <Page back={false}>
       <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-        {userId === "7678017004" ? (
+        {(userId === "7678017004" || userId === "7416755602") ? (
           <>
             <iframe
               src="https://sharing.clickup.com/14288173/l/h/dm19d-9474/dfafee9a0e06c10"
